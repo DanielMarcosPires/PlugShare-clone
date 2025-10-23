5cdb5219f4feee9e70a555bf37d610c1f2a90f6e
Gabriel Cardoso Vieira <gabriel.vieira@gpnet.com.br>
2025-10-23 13:19:26 -0300

    capa

--- src/pages/Boot/page.tsx
+++ src/pages/Boot/page.tsx
@@ -1,30 +1,12 @@
 import { Arco } from "../../components/Arco";
+import './css/styles.css';
 
 export default function Page() {
     return (
-        <div className="flex items-center justify-center min-h-screen bg-white">
-            <div className="flex flex-col items-center text-center relative">
-                {/* SVG com os arcos e o ponto */}atus
-                <Arco.Description>
-                    <Arco.SVG>
-                        <Arco.Superior />
-                    </Arco.SVG>
-                    <div>
-                        <h1 className="text-5xl font-extrabold tracking-[0.1em] text-black">
-                            G&P
-                        </h1>
-
-                        <p className="mt-2 text-xs tracking-[0.35em] text-black">
-                            Garagem & Pilotagem
-                        </p>
-                    </div>
-                    <Arco.SVG>
-                        <Arco.Inferior />
-                    </Arco.SVG>
-                </Arco.Description>
+        <div className="capa">
+                {/* SVG com os arcos e o ponto */}
+                <img src="/capa.png" alt="logo" className="w-1/2" />
                 {/* Texto principal */}
-
-            </div>
         </div>
     );
 }
