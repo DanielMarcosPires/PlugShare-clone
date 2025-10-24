a87222d0aa56ae5546f62f41f18a5f09230ac86c
Gabriel Cardoso Vieira <gabriel.vieira@gpnet.com.br>
2025-10-24 13:14:00 -0300

    produção para converter o codigo

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,6 +3,9 @@ import { Dashboard } from './pages/Dashboard/page'
 import BuscaDeCarro from './pages/BuscaDeCarro/page'
 import BrandSearch from './pages/BrandSearch/page'
 import {page as Boot} from './pages/Boot/page'
+import Sing from './pages/Singup/Sing'
+import Singin from './pages/Singin/Singin'
+import Opt from './pages/Opt/Opt'
 
 //Daniel é o responsavel para configurar as rotas
 
@@ -10,6 +13,9 @@ export default function Router() {
   return (
     <Routes>
       <Route path='/' element={<Boot/>} />
+      <Route path='/signup' element={<Sing/>} />
+      <Route path='/signin' element={<Singin/>} />
+      <Route path='/opt' element={<Opt/>} />
       <Route path='/dashboard' element={<Dashboard/>} />
       <Route path='/brand' element={<BrandSearch/>} />
     </Routes>
