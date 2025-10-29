aba596463a45472e71e06ef11a84cb378d15a065
Humberto.Freitas <sansunghumberto13@gmail.com>
2025-10-29 13:28:27 -0300

    hoje  atualizamos  as paginas

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ import { WebProvider } from "./context/Slide.tsx";
 import App from "./app.tsx";
 import Profile from "./pages/Dashboard/page/Profile/page.tsx";
 import { AddEvVehicle } from "./pages/AddEvVehicle/page.tsx";
-import { CvNetwork } from "./pages/Cv Network/page.tsx";
+
 import { EVNetworkSetView } from "./pages/EVNetworkSetView/page.tsx";
 
 /**
@@ -27,7 +27,6 @@ const router = createBrowserRouter([
       { path: "/", element: <BuscaDeCarro /> },
       { path: "brand", element: <BrandSearch /> },
       { path: "add-ev-vehicle", element: <AddEvVehicle /> },
-      { path: "cv-network", element: <CvNetwork /> },
       { path: "ev-network-set-view", element: <EVNetworkSetView /> },
       {
         path: "dashboard",
